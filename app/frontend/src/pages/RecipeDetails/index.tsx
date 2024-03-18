d8968c311020721eabcdd163d1fa5cdd056d8e0c
Wesley Mendes <wesleymendes123321@gmail.com>
2024-03-18 16:12:16 -0300

    Stylezation - Recipe Details

--- app/frontend/src/pages/RecipeDetails/index.tsx
+++ app/frontend/src/pages/RecipeDetails/index.tsx
@@ -13,7 +13,6 @@ import Context from '../../context/Context';
 import UserInfoContext from '../../context/UserInfo/UserInfoContext';
 import useFetch from '../../hooks/useFetch';
 
-
 export default function RecipeDetails() {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -28,10 +27,10 @@ export default function RecipeDetails() {
   const recipe = data;
 
   const inProgressURL = `http://localhost:3001${route}/inprogress/${id}?user=${userId}`;
-  const inProgress = useFetch(inProgressURL);  
+  const inProgress = useFetch(inProgressURL);
 
   const buttonText = inProgress?.data?.message ? 'Start recipe' : 'Continue recipe';
-  
+
   const startInProgressURL = `http://localhost:3001${route}/inprogress`;
   const reqBody = route === '/meals'
     ? { userId: profile?.data?.id, mealId: Number(id) }
@@ -54,17 +53,17 @@ export default function RecipeDetails() {
     <S.Main>
       {recipe && !loadingNextPage && route === '/meals' ? (
         <MealCard
-          recipeData={recipe}
-          handleInProgress={handleInProgress}
-          buttonText={buttonText}
+          recipeData={ recipe }
+          handleInProgress={ handleInProgress }
+          buttonText={ buttonText }
         />
       ) : null}
 
       {recipe && !loadingNextPage && route === '/drinks' ? (
         <DrinkCard
-          recipeData={recipe}
-          handleInProgress={handleInProgress}
-          buttonText={buttonText}
+          recipeData={ recipe }
+          handleInProgress={ handleInProgress }
+          buttonText={ buttonText }
         />
       ) : null}
 
