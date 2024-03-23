35d58a53429379da01262de44d6954070373683b
Gabriel Muniz <gabrielmfd@gmail.com>
2024-03-22 21:18:04 -0300

    Feat: Frontend => Agora aparece um relógio no card das receitas que estiverem em progresso

--- app/frontend/src/components/RecipesMiniCard/RecipesMiniCard.tsx
+++ app/frontend/src/components/RecipesMiniCard/RecipesMiniCard.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/jsx-max-depth */
+import { useContext } from 'react';
 import { DrinkType, MealType } from '../../type';
-import {
-  Div,
-  Img,
-} from './Card.styles';
+import * as S from './Card.styles';
 import CardText from './CardText';
+import orangeClock from '../../assets/Icons/orangeClock.png';
+import whiteClock from '../../assets/Icons/whiteClock.png'
+
+import UserInfoContext from '../../context/UserInfo/UserInfoContext';
+import useFetch from '../../hooks/useFetch';
 
 type RecipesMiniCardProps = {
   recipe: MealType | DrinkType
@@ -13,13 +16,30 @@ type RecipesMiniCardProps = {
 };
 
 function RecipesMiniCard({ recipe, path, index }: RecipesMiniCardProps) {
-  const title = path === '/meals' ? recipe.strMeal : recipe.strDrink;
+  const { profile } = useContext(UserInfoContext);
+  const userId = profile?.data?.id
+  
+  const id = path === '/meals' ? recipe.idMeal : recipe.idDrink;  
   const thumbnail = path === '/meals' ? recipe.strMealThumb : recipe.strDrinkThumb;
+
+  const inProgressURL = `http://localhost:3001${path}/inprogress/${id}?user=${userId}`;
+  const { data } = useFetch(inProgressURL);
+  if (!data) {
+    return undefined;
+  }
+  const isInProgress = data?.message ? false : true;  
   return (
-    <Div data-testid={ `${index}-recipe-card` }>
-      <Img data-testid={ `${index}-card-img` } src={ thumbnail } alt={ title } />
-      <CardText recipe={ recipe } path={ path } index={ index } />
-    </Div>
+    <S.Div data-testid={ `${index}-recipe-card` }>
+      <S.Img src={thumbnail}>
+        <img src={isInProgress ? orangeClock : ''} alt="" />
+      </S.Img>      
+      <CardText 
+        recipe={ recipe } 
+        path={ path } 
+        index={ index }
+        isInProgress={isInProgress} 
+      />
+    </S.Div>
   );
 }
 
