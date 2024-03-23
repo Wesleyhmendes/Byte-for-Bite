3a47f77491e1441cdc7228a0b1a458717d0a2178
Wesley Mendes <wesleymendes123321@gmail.com>
2024-03-23 11:30:03 -0300

    Feat. Aside navigation menu icons

--- app/frontend/src/components/AsideDesktopMenu/AsideDesktopMenu.styles.tsx
+++ app/frontend/src/components/AsideDesktopMenu/AsideDesktopMenu.styles.tsx
@@ -11,36 +11,52 @@ export const Section = styled.section`
   font-weight: 500;
   gap: 2rem;
   height: 70vh;
-  padding-left: 2rem;
+  padding-left: 1rem;
   width: 15%;
-
-  .active {
-    border-bottom: 2px solid #FE5B15;
-    color: #FE5B15;
-  }
 
   div {
     h2 {
       font-size: x-large;
       color: #b0b0b0;
     }
-
-    display: flex;
-    flex-direction: column;
     gap: 1rem;
-
-    button {
-      border: none;
-      display: flex;
-      flex-direction: column;
-      gap: 0.3rem;
-      transition: ease 0.3s;
-      width: fit-content;
-
-      &:hover {
-        color: #FE5B15;
-      }
-    }
   }
 }
+
+  @media (min-width: 1444px) {
+    padding-left: 2rem;
+  }
+`;
+
+export const ButtonDiv = styled.div`
+  align-items: center;
+  display: flex;
+  margin: 1rem 0;
+  width: 100%;
+  
+  button {
+    display: flex;
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+    transition: ease 0.3s;
+    width: fit-content;
+    
+    &:hover {
+      color: #FE5B15;
+    }
+
+    .active {
+      border-bottom: 2px solid #FE5B15;
+      color: #FE5B15;
+    }
+  }
+
+  p {
+    text-align: left;
+  }
+
+  img {
+    width: 29px;
+  }
 `;
--- app/frontend/src/components/AsideDesktopMenu/AsideDesktopMenu.tsx
+++ app/frontend/src/components/AsideDesktopMenu/AsideDesktopMenu.tsx
@@ -1,4 +1,8 @@
 import { useLocation, useNavigate } from 'react-router-dom';
+import verifiedIcon from '../../assets/Icons/AsideMenuDesktop/done_icon.png';
+import favoriteIcon from '../../assets/Icons/AsideMenuDesktop/favorite_icon.png';
+import glassIcon from '../../assets/Icons/AsideMenuDesktop/glass-icon32.png';
+import plateIcon from '../../assets/Icons/AsideMenuDesktop/plate-icon32.png';
 import * as S from './AsideDesktopMenu.styles';
 
 export default function AsideDesktopMenu() {
@@ -8,31 +12,43 @@ export default function AsideDesktopMenu() {
     <S.Section>
       <div>
         <h2>Pages</h2>
-        <button
-          className={ url.endsWith('drinks') ? 'active' : '' }
-          onClick={ () => navigate('/drinks') }
-        >
-          Drinks
-        </button>
-        <button
-          className={ url.endsWith('meals') ? 'active' : '' }
-          onClick={ () => navigate('/meals') }
-        >
-          Meals
-        </button>
+        <S.ButtonDiv>
+          <img className="glass" src={ glassIcon } alt="icon of a glass" />
+          <button
+            className={ url.endsWith('drinks') ? 'active' : '' }
+            onClick={ () => navigate('/drinks') }
+          >
+            <p>Drinks</p>
+          </button>
+        </S.ButtonDiv>
+        <S.ButtonDiv>
+          <img src={ plateIcon } alt="icon of a plate" />
+          <button
+            className={ url.endsWith('meals') ? 'active' : '' }
+            onClick={ () => navigate('/meals') }
+          >
+            <p>Meals</p>
+          </button>
+        </S.ButtonDiv>
       </div>
       <div>
         <h2>Filter</h2>
-        <button
-          onClick={ () => navigate('/done-recipes') }
-        >
-          Done Recipes
-        </button>
-        <button
-          onClick={ () => navigate('/favorite-recipes') }
-        >
-          Favorite Recipes
-        </button>
+        <S.ButtonDiv>
+          <img src={ verifiedIcon } alt="verified icon" />
+          <button
+            onClick={ () => navigate('/done-recipes') }
+          >
+            <p>Done Recipes</p>
+          </button>
+        </S.ButtonDiv>
+        <S.ButtonDiv>
+          <img src={ favoriteIcon } alt="verified icon" />
+          <button
+            onClick={ () => navigate('/favorite-recipes') }
+          >
+            <p>Favorite Recipes</p>
+          </button>
+        </S.ButtonDiv>
       </div>
     </S.Section>
   );
