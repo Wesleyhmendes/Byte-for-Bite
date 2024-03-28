419cbc2cf8b330f42921caf996437b7e65a74033
Wesley Mendes <wesleymendes123321@gmail.com>
2024-03-28 19:31:51 -0300

    Fix. Delete build folder

--- app/backend/build/database/seeders/999999999999999.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    up: async (queryInterface) => {
-    },
-    down: async (queryInterface) => {
-    },
-};
-//# sourceMappingURL=999999999999999.js.map
