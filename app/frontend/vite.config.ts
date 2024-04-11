02f4c224fc273ca346ba4afe96c3d89bd2c133fa
Wesley Mendes <wesleymendes123321@gmail.com>
2024-04-10 22:36:07 -0300

    change enviroment variables frontend

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT || 3000),
+  },
   test: {
     globals: true,
     environment: 'jsdom',
